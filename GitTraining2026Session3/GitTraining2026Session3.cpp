4214b115c342fd0d361d2c435ccdfde773dde0f9
Farhan Hussain <farhan08822@outlook.com>
2026-09-20 21:00:54 -0400

    TTT2025W-22 added cout

--- GitTraining2026Session3/GitTraining2026Session3.cpp
+++ GitTraining2026Session3/GitTraining2026Session3.cpp
@@ -10,6 +10,7 @@ int main()
     std::cout << "This is heather.\n";
     std::cout << "Change on remote from prof\n";
     std::cout << "Hello from Christian\n";
+    std::cout << "yo idk how to feel about blue cheese\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
